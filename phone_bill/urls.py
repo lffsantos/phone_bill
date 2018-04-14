67258ad26d7cc2a6facb2af3a2bcb2cd1d87077d
Lucas <lffsantos@gmail.com>
2018-04-14 15:32:37 -0300

    added template home for added documents in the future

--- phone_bill/urls.py
+++ phone_bill/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.urls import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='core/itworks.html'), name='home'),
     url(r'^api/v1/', include('phone_bill.core.urls')),
 ]
