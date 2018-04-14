e52eea1b8782b10b6fb2006f7a04f8662306d332
Lucas <lffsantos@gmail.com>
2018-04-13 23:30:13 -0300

    remove admin url

--- phone_bill/urls.py
+++ phone_bill/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-from django.contrib import admin
 from django.urls import include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('phone_bill.core.urls')),
 ]
