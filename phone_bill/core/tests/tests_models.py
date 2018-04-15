09115d9e9ed29a32cf427560c7a4832a16c511ce
Lucas <lffsantos@gmail.com>
2018-04-15 00:45:32 -0300

    improve test

--- phone_bill/core/tests/tests_models.py
+++ phone_bill/core/tests/tests_models.py
@@ -1,5 +1,6 @@
 import json
 from _datetime import datetime
+from model_mommy import mommy
 
 from parameterized import parameterized
 from django.test import TestCase
@@ -24,56 +25,38 @@ class TestCall(TestCase):
 
 class TestCallBilling(TestCase):
 
+    def setUp(self):
+        self.tariff = mommy.make(Tariff, start_time='06:00', end_time='22:00',
+                                 call_charge=0.09, standing_charge=0.36)
     @parameterized.expand([
         (json.dumps({
             'start': '12/12/2017 15:07:13',
             'duration': 463,
-            'tariff': {
-                'start_time': '06:00', 'end_time': '22:00',
-                'call_charge': 0.09, 'standing_charge': 0.36
-            },
             'expected_price': 0.99
         })),
         (json.dumps({
             'start': '12/12/2017 22:47:56',
             'duration': 180,
-            'tariff': {
-                'start_time': '06:00', 'end_time': '22:00',
-                'call_charge': 0.09, 'standing_charge': 0.36
-            },
             'expected_price': 0.36,
         })),
         (json.dumps({
             'start': '12/12/2017 21:57:13',
             'duration': 823,
-            'tariff': {
-                'start_time': '06:00', 'end_time': '22:00',
-                'call_charge': 0.09, 'standing_charge': 0.36
-            },
             'expected_price': 0.54,
         })),
         (json.dumps({
             'start': '12/12/2017 4:57:13',
             'duration': 4423,
-            'tariff': {
-                'start_time': '06:00', 'end_time': '22:00',
-                'call_charge': 0.09, 'standing_charge': 0.36
-            },
             'expected_price': 1.26,
         })),
         (json.dumps({
             'start': '12/12/2017 21:57:13',
             'duration': 87223,
-            'tariff': {
-                'start_time': '06:00', 'end_time': '22:00',
-                'call_charge': 0.09, 'standing_charge': 0.36
-            },
             'expected_price': 86.94,
         }))
     ])
     def test_price_call(self, data):
         data = json.loads(data)
         start_date = datetime.strptime(data['start'], '%d/%m/%Y %H:%M:%S')
-        tariff = Tariff.objects.create(**data['tariff'])
-        price = CallBilling.price_call(start_date, data['duration'], tariff)
+        price = CallBilling.price_call(start_date, data['duration'], self.tariff)
         self.assertEqual(str(data['expected_price']), "{:.2f}".format(price))
