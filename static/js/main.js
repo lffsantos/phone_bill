0ee0221046846e87787bfb64bc1ace8495c1a504
Lucas <lffsantos@gmail.com>
2018-04-15 20:34:20 -0300

    basic front end for search phone bill

---
+++ static/js/main.js
@@ -0,0 +1,42 @@
+$( "#bill" ).click(function( event ) {
+    clear();
+    var source = $('#inputSource').val();
+    var period = $('#periodDate').val();
+    if(period == undefined){
+        period = ''
+    }
+    $.ajax({
+      url: "/api/v1/get_phone_bill/?source="+source+"&period="+period,
+      success: function(data) {
+          if (data['calls'].length == 0) {
+              $('#notAccount').show();
+              $('#detailAccount').hide();
+          }
+          else {
+              $('#notAccount').hide();
+              $('#detailAccount').show();
+              $.each(data['calls'], function (key, value) {
+                  var detail = '<tr>' +
+                      '<td>' + key + '</td>' +
+                      '<td>' + value['destination'] + '</td>' +
+                      '<td>' + value['start_date'] + '</td>' +
+                      '<td>' + value['start_time'] + '</td>' +
+                      '<td>' + value['call_duration'] + '</td>' +
+                      '<td>' + value['price'] + '</td>' +
+                      '</tr>';
+                  $('table>tbody').append(detail);
+              });
+              $('#phoneNumber').html(source);
+              $('#period').html(data['period']);
+              $('#amountAccount').html(data['amount']);
+          }
+      }
+    });
+    event.preventDefault();
+});
+function clear(){
+  $('#phoneNumber').html('');
+  $('#period').html('');
+  $('#amountAccount').html('');
+  $('table>tbody').html('');
+}
