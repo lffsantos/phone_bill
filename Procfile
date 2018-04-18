0bf29e049f4eb68cecf9cc32c9091d248ef2e284
Lucas <lffsantos@gmail.com>
2018-04-18 00:36:45 -0300

    test scheduler heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn phone_bill.wsgi --log-file -
+clock: python generate_account.py
---
+++ generate_account.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.db.models import Q
+from django.utils import timezone
+from phone_bill.core.models import Call, PhoneBill
+
+
+scheduler = BackgroundScheduler()
+
+
+@scheduler.scheduled_job('cron', id='close_account', day=1, minute=1)
+def save_account():
+    # process account of last month finish
+    now = timezone.now() - timedelta(days=1)
+    month, year = now.month, now.year
+    call_ids = Call.objects.filter(
+        (Q(timestamp__month=month) & Q(timestamp__year=year)),
+        type_call='end'
+    ).values_list('call_id', flat=True)
+    calls = Call.objects.filter(
+        call_id__in=call_ids
+    ).order_by('call_id', 'timestamp')
+    PhoneBill.objects.generate_accounts(calls, month, year)
+
+
+scheduler.start()
